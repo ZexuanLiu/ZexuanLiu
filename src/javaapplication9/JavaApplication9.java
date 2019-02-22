fb74e46eaf78f2f6b4e67d83bc81067ffd9390f0
ZexuanLiu <liuzex@sheridancollege.ca>
2019-02-23 01:27:16 +0800

    “Finished adding code for 3 and 4 to CardTrick.java”

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,21 @@
+/*
+ * Zexuan Liu
+ * ID: 991496508
+ * SYST10199- Web Programming
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author Administrator
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
